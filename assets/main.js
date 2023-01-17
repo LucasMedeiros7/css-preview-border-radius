a940dc79dbb265275bac3f7f06d147ba86d04f0d
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-01-16 23:59:09 -0300

    fix: bug  no apply border radius when passed two numbers

--- assets/main.js
+++ assets/main.js
@@ -10,30 +10,30 @@ function modifyBorderRadius(input) {
     'bottom-left': 'borderBottomLeftRadius',
   };
 
-  input.addEventListener('input', event => { 
-    let userInput = (+event.target.value).toFixed(3);
+  input.addEventListener('input', event => {
+    let userInput = event.target.value;
     if (userInput.length > 3) {
       userInput = userInput.substring(0, 3);
     }
     const direction = borderDirection[event.target.id];
-    generateBorderRadiusValues(direction, userInput)
+    generateBorderRadiusValues(direction, userInput);
     $box.style[direction] = `${userInput}px`;
   });
 }
 
 function generateBorderRadiusValues(direction, value) {
   const borderDirectionValues = {
-    'borderTopLeftRadius': 0,
-    'borderTopRightRadius': 1,
-    'borderBottomRightRadius': 2,
-    'borderBottomLeftRadius': 3,
-  }
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 1,
+    borderBottomRightRadius: 2,
+    borderBottomLeftRadius: 3,
+  };
   const $borderRadiusValues = document.querySelector('#border-radius-values');
-  const listDirectionValues = $borderRadiusValues.innerText.split(' ')
-  if(value <= 0 ) {
-    listDirectionValues[borderDirectionValues[direction]] = 0
+  const listDirectionValues = $borderRadiusValues.innerText.split(' ');
+  if (value <= 0) {
+    listDirectionValues[borderDirectionValues[direction]] = 0;
   } else {
-    listDirectionValues[borderDirectionValues[direction]] = `${Math.round(value)}px` 
+    listDirectionValues[borderDirectionValues[direction]] = `${value}px`;
   }
-  $borderRadiusValues.innerText = listDirectionValues.join(' ')
+  $borderRadiusValues.innerText = listDirectionValues.join(' ');
 }
